e4fbefe177d3337815859237ceac40bca644fefa
catce2020111lmat0259-pixel <catce2020111lmat0259@aluno.ifpi.edu.br>
2026-06-25 11:27:02 -0300

    Atualiza index

--- routes/index_router.py
+++ routes/index_router.py
@@ -3,4 +3,4 @@ from flask import Flask
 from controllers import index_controller
 
 def adicionar_rotas(app: Flask):
-    app.add_url_rule(rule="/fine/home", endpoint="home", view_func=index_controller.home, methods=["GET"])
+    app.add_url_rule(rule="/", endpoint="home", view_func=index_controller.home, methods=["GET"])
